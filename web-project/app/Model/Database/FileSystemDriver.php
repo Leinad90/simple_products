6bf6923acc10620170a9a4018cd8e214667f919d
Leinad90 <daniel.hejduk@gmail.com>
2026-05-17 21:45:57 +0200

    Some stability changes

--- web-project/app/Model/Database/FileSystemDriver.php
+++ web-project/app/Model/Database/FileSystemDriver.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Model\Database;
 
+use Nette\IOException;
+use Nette\Utils\FileSystem;
+
 class FileSystemDriver
 {
 
@@ -16,6 +19,14 @@ class FileSystemDriver
     public function incrementReadCounter(string $id): void
     {
         $path = $this->getCounterPath($id);
+
+        // ensure directory exists
+        $dir = dirname($path);
+        try {
+            FileSystem::createDir($dir);
+        } catch (IOException $e) {
+           throw new FileSystemException('Unable to create directory ' . $dir, previous: $e);
+        }
 
         try {
             $file = new \SplFileObject($path, 'c+');
@@ -32,16 +43,17 @@ class FileSystemDriver
             $contents = $this->readFileContents($file);
             $count = 0;
             if ($contents !== '' && trim($contents) !== '') {
-                $count = (int) trim($contents);
+                $count = (int)trim($contents);
             }
 
             $count++;
-            $data = (string) $count . PHP_EOL;
+            $data = (string)$count . PHP_EOL;
             $this->rewriteFile($file, $data);
 
             $file->flock(LOCK_UN);
         } finally {
             unset($file);
+            // get sure to close file even if exception occurs
         }
     }
 
@@ -52,10 +64,9 @@ class FileSystemDriver
 
     protected function rewriteFile(\SplFileObject $file, string $data): void
     {
-        if (!$file->ftruncate(0)) {
+        if(!$file->ftruncate(0)) {
             throw new FileSystemException('Unable to truncate file');
         }
-
         $file->rewind();
 
         $bytes = $file->fwrite($data);
@@ -67,19 +78,23 @@ class FileSystemDriver
             throw new FileSystemException('Unable to write whole data to file');
         }
 
-        if (!$file->fflush()) {
+        if(!$file->fflush()) {
             throw new FileSystemException('Unable to flush file');
-        }
+        };
     }
 
+    /**
+     * @throws FileSystemException
+     */
     private function readFileContents(\SplFileObject $file): string
     {
         $file->rewind();
-        $contents = '';
-        while (!$file->eof()) {
-            $contents .= $file->fgets();
+        try {
+            $line = $file->fgets();
+        } catch (\RuntimeException $e) {
+            throw new FileSystemException('Unable to read file', previous: $e);
         }
-        return $contents;
+        return $line;
     }
 }
 
@@ -87,4 +102,3 @@ class FileSystemException extends \RuntimeException
 {
 
 }
-
